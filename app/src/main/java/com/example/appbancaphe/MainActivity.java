72f9b0ffb35d35c237ec40c0b9999050d8069b74
MeoXui <130195222+MeoXui@users.noreply.github.com>
2024-05-19 00:14:46 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/appbancaphe/MainActivity.java
+++ app/src/main/java/com/example/appbancaphe/MainActivity.java
@@ -24,70 +24,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mAuth = FirebaseAuth.getInstance();
         TextInputEditText userdnhap = findViewById(R.id.tieNhapUsername);
         TextInputEditText mkdnhap = findViewById(R.id.tieNhapmatkhau);
 
         TextView btnsignup = findViewById(R.id.btnsignup);
-
         TextView quenmk = findViewById(R.id.forgot);
         Button signin= findViewById(R.id.btnDangnhap);
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = userdnhap.getText().toString();
-                String password = mkdnhap.getText().toString();
-                if (email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Vui lòng nhập đủ thông tin đăng nhập", Toast.LENGTH_SHORT).show();
-                } else {
-                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d("Main", "createUserWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
 
-                                Toast.makeText(getApplicationContext(), "Đăng nhập thành công", Toast.LENGTH_LONG).show();
-                                startActivity(new Intent(MainActivity.this, TrangChu.class));
-                                finish();
+        signin.setOnClickListener(v -> {
+            String email = userdnhap.getText().toString();
+            String password = mkdnhap.getText().toString();
 
-                                //updateUI(user);
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("Main", "createUserWithEmail:failure", task.getException());
-                                Toast.makeText(MainActivity.this, "Vui lòng kiểm tra lại tài khoản hoặc mậy khẩu",
-                                        Toast.LENGTH_SHORT).show();
-                                //updateUI(null);
-                            }
-                        }
-                    });
-                }
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập đủ thông tin đăng nhập",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Main", "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(getApplicationContext(), "Đăng nhập thành công",
+                                Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(MainActivity.this, TrangChu.class));
+                        finish();
+
+                        //updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Main", "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(MainActivity.this, "Vui lòng kiểm tra lại tài khoản hoặc mậy khẩu",
+                                Toast.LENGTH_SHORT).show();
+
+                        //updateUI(null);
+                    }
+                });
             }
         });
 
-        btnsignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Dki.class);
-                startActivity(intent);
-                finish();
-            }
+        btnsignup.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Dki.class);
+            startActivity(intent);
+            finish();
         });
-        quenmk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = userdnhap.getText().toString();
-                //phát sinh lỗi ở đây - Huynk PH38086
-                //chức năng quên mật khẩu bị lỗi - Huynk PH38086
-                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(MainActivity.this, "Vui lòng kiểm tra hộp thư email", Toast.LENGTH_SHORT).show();
-                        }else {
-                            Toast.makeText(MainActivity.this, "Lỗi gửi email", Toast.LENGTH_SHORT).show();
-                        }
+        
+        quenmk.setOnClickListener(v -> {
+            String email = userdnhap.getText().toString();
+            if (email.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập email của bạn",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Vui lòng kiểm tra hộp thư email",
+                                Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(MainActivity.this, "Lỗi gửi email", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
