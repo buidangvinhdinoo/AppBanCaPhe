b9f9696a504b077a8f9ba9e9a36ed265daaa4f97
Hungkn32 <hungknph34593@fpt.edu.vn>
2024-05-20 16:56:08 +0700

    Thêm Splash screen

---
+++ app/src/main/java/com/example/appbancaphe/Welcom.java
@@ -0,0 +1,31 @@
+package com.example.appbancaphe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Welcom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcom);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Chuyển sang MainActivity
+                Intent intent = new Intent(Welcom.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // 3000 milliseconds = 3 seconds
+
+    }
+}
