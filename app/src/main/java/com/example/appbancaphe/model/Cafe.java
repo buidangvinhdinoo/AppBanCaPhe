e86c8138475e3322aef830e5d0598e3555b6bb1c
MeoXui <130195222+MeoXui@users.noreply.github.com>
2024-05-23 22:36:10 +0700

    .

--- app/src/main/java/com/example/appbancaphe/model/Cafe.java
+++ app/src/main/java/com/example/appbancaphe/model/Cafe.java
@@ -7,5 +7,14 @@ public class Cafe {
     public double don_gia;
     public int trang_thai; //ban het, dang ban
 
-    //
+    public Cafe() {
+    }
+
+    public Cafe(String _id, int loai, int kich_co, double don_gia, int trang_thai) {
+        this._id = _id;
+        this.loai = loai;
+        this.kich_co = kich_co;
+        this.don_gia = don_gia;
+        this.trang_thai = trang_thai;
+    }
 }
