5730b7062869d899020fa12b8fbdca9b81f1c197
Vinh <buidangvinhdino@gmail.com>
2024-06-01 10:30:42 +0700

    lamgiaodiengiohangvasuamanhome

--- app/src/main/java/com/example/appbancaphe/MainActivity.java
+++ app/src/main/java/com/example/appbancaphe/MainActivity.java
@@ -1,57 +1,67 @@
 package com.example.appbancaphe;
 
 import android.os.Bundle;
-import android.view.Window;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.appbancaphe.databinding.ActivityMainBinding;
-import com.example.appbancaphe.fragment.Cart;
+import com.example.appbancaphe.fragment.Account;
 import com.example.appbancaphe.fragment.Home;
 import com.example.appbancaphe.fragment.Product;
-import com.example.appbancaphe.fragment.Account;
+import com.example.appbancaphe.fragment.Cart;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
-
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    public static BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.pageTrangChu);
+        loadFragment(new Home());
 
 
-        replaceFragment(new Home());
 
-        binding.bottomnav.setOnItemSelectedListener(menuItem -> {
-            int itemId = menuItem.getItemId();
-            Fragment selectedFragment = null;
 
-            if (itemId == R.id.btnhome) {
-                selectedFragment = new Home();
-            } else if (itemId == R.id.btnproduct) {
-                selectedFragment = new Product();
-            } else if (itemId == R.id.btncart) {
-                selectedFragment = new Cart();
-            } else if (itemId == R.id.btntaikhoan) {
-                selectedFragment = new Account();
-            }
-
-            if (selectedFragment != null) {
-                replaceFragment(selectedFragment);
-            }
-
-            return true;
-        });
     }
 
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentlayout, fragment);
-        fragmentTransaction.commit();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            if(item.getItemId() == R.id.pageTrangChu){
+                Home frgHome = new Home();
+                loadFragment(frgHome);
+
+            }else if(item.getItemId() == R.id.pageSanPham){
+                Product frgSp = new Product();
+                loadFragment(frgSp);
+
+            }
+            else if(item.getItemId() == R.id.pageBanHang){
+                Cart frgst = new Cart();
+                loadFragment(frgst);
+
+            }
+            else if(item.getItemId() == R.id.pageTaiKhoan){
+                Account frgacc = new Account();
+                loadFragment(frgacc);
+
+            }
+        return true;
+            
+
+
+
+    }
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
